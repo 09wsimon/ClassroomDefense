45c3acae9287ff565b6535b3eb497b9e0001884b
Nick Lubrano <Nick Lubrano@130.203.91.21>
2015-09-16 20:27:44 -0400

    hi

--- ClassroomDefense/src/classroomdefense/ClassroomDefense.java
+++ ClassroomDefense/src/classroomdefense/ClassroomDefense.java
@@ -15,7 +15,7 @@ public class ClassroomDefense {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Helo");
+        System.out.println("Hello");
         System.out.println("Is it working?");
     }
     
